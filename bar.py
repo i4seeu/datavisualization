f6a4b0d4ed1ae03f897cd120654cab4bfb9338ab
Noordeen Malango <noordinmalango@gmail.com>
2019-11-26 20:04:43 +0200

    bar grap

--- bar.py
+++ bar.py
@@ -1,1 +1,6 @@
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
+
+movies = ['Annie Hall', 'Ben-Hur', 'Casablanca', 'Gandhi', 'West side story']
+num_oscars = [5,11,3,8,10]
+
+#bars are by default with 0.8 , we ll add 0.1, to the left coordinates so that each bar is centered
